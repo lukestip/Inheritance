db57092779b03d225c54d19b2818b2c7e0b96af6
Luke <luke_stipanovic1@baylor.edu>
2023-02-20 23:38:38 -0600

    class commit 2-20-23

--- f_polymorphism_demo2.py
+++ f_polymorphism_demo2.py
@@ -2,6 +2,7 @@
 
 import f_animals as animals
 
+
 def main():
     # Create an Mammal object, a Dog object, and
     # a Cat object.
@@ -19,18 +20,20 @@ def main():
     print()
     show_mammal_info(cat)
     print()
-    show_mammal_info('I am a string')
+    show_mammal_info('Zebra')  # a string, not an animal object
 
 # The show_mammal_info function accepts an object
 # as an argument, and calls its show_species
 # and make_sound methods.
+
 
 def show_mammal_info(creature):
     if isinstance(creature, animals.Mammal):
         creature.show_species()
         creature.make_sound()
     else:
-        print('That is not a Mammal!')
+        print(f'"{creature}" is not a Mammal!')
+
 
 # Call the main function.
 main()
