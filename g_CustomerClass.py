7cce20d269b3befd01606bb01183a98de866dff1
Luke <luke_stipanovic1@baylor.edu>
2023-02-22 13:28:09 -0600

    commit in class 2-22-23

--- g_CustomerClass.py
+++ g_CustomerClass.py
@@ -6,9 +6,18 @@ class Person():
         self.__phoneNum = phoneNum
 
     def print_person(self):
-        print(self.__name)
-        print(self.__address)
-        print(self.__phoneNum)
+        print(f'Name: {self.__name}')
+        print(f'Addr: {self.__address}')
+        print(f'Phone: {self.__phoneNum}')
+
+    def get_name(self):
+        return self.__name
+
+    def get_address(self):
+        return self.__address
+
+    def get_phone(self):
+        return self.__phoneNum
 
 
 class Customer(Person):
@@ -19,6 +28,15 @@ class Customer(Person):
         self.__mailList = mailList
 
     def print_person(self):
-        Person.print_person(self)
-        print(self.__custNum)
-        print(self.__mailList)
+        Person.print_person(self)  # print_person()
+        print(f'Cust Num: {self.__custNum}')
+        if self.__mailList:
+            print('On Mailing List: Yes')
+        else:
+            print('On Mailing List: No')
+
+    def get_custNum(self):
+        return self.__custNum
+
+    def get_mailList(self):
+        return self.__mailList
--- g_CustomerProgram.py
+++ g_CustomerProgram.py
@@ -5,4 +5,5 @@ joe = cc.Person('Joe', '1 Bear Pl', '254-555-7045')
 luke = cc.Customer('Luke', '2 Bear Pl', '972-555-7045', '01', False)
 
 joe.print_person()
+print()
 luke.print_person()
